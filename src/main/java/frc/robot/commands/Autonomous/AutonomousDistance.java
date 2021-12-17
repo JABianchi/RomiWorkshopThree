8ca259c8b8b18c4884cf2fb57a2aa76b849ff172
TheRoyalTea <np768@scarletmail.rutgers.edu>
2021-12-17 09:07:57 -0500

    First Commit

---
+++ src/main/java/frc/robot/commands/Autonomous/AutonomousDistance.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autonomous;
+
+import frc.robot.commands.DriveDistance;
+import frc.robot.commands.TurnDegrees;
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class AutonomousDistance extends SequentialCommandGroup {
+  /**
+   * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
+   * turn around and drive back.
+   *
+   * @param drivetrain The drivetrain subsystem on which this command will run
+   */
+  /* [INFO 1A] There are many command groups. When we use sequencial command groups, one command runs
+  after the previous one is done. For every command group like always, ask for all the information
+  we need but don't have in the parameters, here we need the drivetrain to move. */
+
+  public AutonomousDistance(Drivetrain drivetrain) {
+    /* [Info 1B] Always write the addCommands(); function and inside each command
+    gets called with new [NameOfCommand]([All The Parameters]); and each command is
+    seperated by a , */
+    addCommands(
+        new DriveDistance(0.5, 10, drivetrain),
+        new TurnDegrees(-0.5, 180, drivetrain),
+        new DriveDistance(0.5, 10, drivetrain));
+  }
+}
---
+++ src/main/java/frc/robot/commands/Autonomous/AutonomousSquare.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autonomous;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutonomousSquare extends SequentialCommandGroup {
+  /** [TASK 1E] Creates a new AutonomousSquare. Here create an autonomous command that
+   * runs the robot in a square path.
+   */
+  public AutonomousSquare() {
+    addCommands();
+  }
+}
